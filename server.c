f2551837bfe37c752111c4d36968902f963f13fa
Ciro S. Costa <ciro.costa@usp.br>
2017-12-31 20:48:52 +0000

    replaces sendfile by pure `fread` to MAXLINEd buffer

--- server.c
+++ server.c
@@ -1,16 +1,15 @@
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/sendfile.h>
 
 #include "./conn.h"
 
 #define LISTEN_BACKLOG 128
-#define MAXLINE 4096
+#define MAXLINE (1 << 12)
 
 int
 init_server_conn(t_conn* connection, int listen_fd)
@@ -45,27 +44,31 @@ init_server_conn(t_conn* connection, int listen_fd)
 int
 work_on_connection(t_conn* connection)
 {
-        int null_fd = open("/dev/null", O_WRONLY);
-        int run = 1;
-        int err = 0;
+	int run = 1;
+        char buf[MAXLINE];
+        size_t n;
 
-        while (run) {
-                err = sendfile(null_fd, connection->fd, NULL, 1024);
-                if (err == -1) {
-                        perror("failed to transmit bytes to null fd - sendfile");
-                        close(null_fd);
+
+	while (run) {
+                n = fread(buf, sizeof(char), MAXLINE, connection->rx);
+                if (n == 0) {
+                        if (feof(connection->rx)) {
+                                return 0;
+                        }
+
+                        perror("failed to read contents - fread");
                         return 1;
                 }
-        }
+	}
 
-        close(null_fd);
-        return 0;
+	return 0;
 }
 
 int
 main()
 {
 	int listen_fd;
+	int err = 0;
 	struct sockaddr_in server_addr;
 
 	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -75,8 +78,35 @@ main()
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(1337);
 
-	bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	listen(listen_fd, LISTEN_BACKLOG);
+	int reuse = 1;
+	if (setsockopt(listen_fd,
+	               SOL_SOCKET,
+	               SO_REUSEADDR,
+	               (const char*)&reuse,
+	               sizeof(reuse)) < 0)
+		perror("setsockopt(SO_REUSEADDR) failed");
+
+#ifdef SO_REUSEPORT
+	if (setsockopt(listen_fd,
+	               SOL_SOCKET,
+	               SO_REUSEPORT,
+	               (const char*)&reuse,
+	               sizeof(reuse)) < 0)
+		perror("setsockopt(SO_REUSEPORT) failed");
+#endif
+
+	err =
+	  bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	if (err == -1) {
+		perror("failed to bind to port 1337 - bind");
+		exit(1);
+	}
+
+	err = listen(listen_fd, LISTEN_BACKLOG);
+	if (err == -1) {
+		perror("failed to transition socket to passive state - listen");
+		exit(1);
+	}
 
 	for (;;) {
 		t_conn connection = { 0 };
